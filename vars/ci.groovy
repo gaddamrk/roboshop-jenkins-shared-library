1f0e065146006db1f7b9149683531f67961abefa
raj <raj>
2023-06-16 16:42:54 +0530

    set roboshop m

---
+++ vars/ci.groovy
@@ -0,0 +1,36 @@
+def call() {
+
+  pipeline {
+
+      agent {
+          label 'workstation'
+      }
+
+      stages {
+
+          stage('compile/build') {
+              steps {
+                  echo ' compile'
+              }
+          }
+
+          stage('unit test') {
+              steps {
+                  echo 'unit test'
+              }
+          }
+
+          stage('quality control') {
+              steps {
+                  echo 'quality control'
+              }
+          }
+
+          stage('upload code to centralized place') {
+              steps {
+                  echo 'upload'
+              }
+          }
+      }
+  }
+}
