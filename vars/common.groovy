a903b457c86753d0328972e4b270a26a6ce08284
raj <raj>
2023-06-20 10:13:09 +0530

    set roboshop m

--- vars/common.groovy
+++ vars/common.groovy
@@ -11,7 +11,7 @@ def compile() {
 def unittests() {
   if (app_lang == "nodejs") {
   //developer is missing unittest cases in our project , he need to add them as best practices, we are skipping to proceed further
-    sh 'npm test || true'
+    sh 'npm test'
   }
   if (app_lang == "maven") {
     sh 'mvn test'
