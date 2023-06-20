56a9cdd88c8fdb32d9977207ed7b7b355decb3f2
raj <raj>
2023-06-20 09:22:25 +0530

    set roboshop m

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,44 +1,48 @@
+
 def call() {
-  try {
-
-    pipeline {
-
-      agent {
-        label 'workstation'
-      }
-      stages {
-        stage('compile/build') {
-          steps {
-            script {
-              common.compile()
+    try {
+        pipeline {
+            agent {
+                label 'workstation'
             }
-          }
-        }
+            stages {
 
-        stage('unit job.xml') {
-          steps {
-            script{
-              common.unittests()
+                stage('compile/build') {
+                    steps {
+                        script {
+                            common.compile()
+                        }
+                    }
+                }
+
+                stage('unit job.xml') {
+                    steps {
+                        script{
+                            common.unittests()
+                        }
+                    }
+                }
+
+                stage('quality control') {
+                    steps {
+                        echo 'quality control'
+                    }
+                }
+
+                stage('upload code to centralized place') {
+                    steps {
+                        echo 'upload'
+                    }
+                }
             }
-          }
         }
-
-        stage('quality control') {
-          steps {
-            echo 'quality control'
-          }
-        }
-
-        stage('upload code to centralized place') {
-          steps {
-            echo 'upload'
-          }
-        }
-      }
+    } catch(Exception e) {
+        common.email('failed')
     }
-  }
- } catch (Exception e) {
-   common.email('failed')
-}
+
 
 }
+
+
+
+
