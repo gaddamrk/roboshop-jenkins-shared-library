bb3088960b3165e3fc19de9fa592db24f322c64c
raj <raj>
2023-06-22 17:32:36 +0530

    set roboshop m

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -25,11 +25,11 @@ def call() {
 
         stage('quality control') {
            environment {
-             sonarqube.user = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
-             sonarqube.pass = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
+             sonar_user = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
+             sonar_pass = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
            }
            steps {
-             sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${sonarqube.user} -Dsonar.password=${sonarqube.pass} -Dsonar.projectKey=cart"
+             sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${sonar_user} -Dsonar.password=${sonar_pass} -Dsonar.projectKey=cart"
 
 
            }
