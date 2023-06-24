66667f8ab898abea338cca8d468abeb431cf8ac0
raj <raj>
2023-06-24 19:10:08 +0530

    set roboshop m

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -14,17 +14,7 @@ def call() {
         common.unittests()
       }
 
-      stage('quality control') {
-        environment {
-          SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\'',
-          SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\'', returnStdout: true).trim()
-          wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-            sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
 
-          }
-
-        }
-      }
       stagge('upload code to centralized place') {
         echo 'upload file'
 
