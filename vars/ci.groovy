261cdffa9991f16583c122596aaf78ab2722bbd6
raj <raj>
2023-06-24 16:22:25 +0530

    set roboshop m

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -16,7 +16,7 @@ def call() {
 
       stage('quality control') {
         environment {
-          SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
+          SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\'', returnStdout: true).trim()
           SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\'', returnStdout: true).trim()
           wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
             sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
@@ -32,6 +32,7 @@ def call() {
 
     }
   } catch (Exception e) {
+    common.email('failed')
 
   }
 
