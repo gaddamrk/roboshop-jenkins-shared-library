d1e342c5ff0d45f7aee1430ce731e7e28feb1f65
raj <raj>
2023-06-24 13:59:40 +0530

    set roboshop m

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -24,28 +24,25 @@ def call() {
         }
 
         stage('quality control') {
+          environment {
+           sonar_user = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
+           //sonar_pass = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
+          }
 
-
-            environment {
-             sonar_user = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
-             //sonar_pass = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
-            }
-            steps {
-
-              script {
-
-                sonar_pass = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\')', returnStdout: true).trim()
-                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${sonar_pass}", var: 'secret']]]) {
-                  sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${sonar_user} -Dsonar.password=${sonar_pass} -Dsonar.projectKey=${component}"
-
-                }
+          steps {
+            script {
+              sonar_pass = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\'', returnStdout: true).trim()
+              wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${sonar_pass}", var: 'secret']]]) {
+                sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${sonar_user} -Dsonar.password=${sonar_pass} -Dsonar.projectKey=${component}"
 
               }
 
-
             }
+
+
           }
         }
+      }
         stage('upload code to centralized place') {
           steps {
             echo 'upload'
