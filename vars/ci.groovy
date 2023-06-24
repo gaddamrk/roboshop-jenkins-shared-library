49366244deb7755a346bc3344af15e7eb6ce038a
raj <raj>
2023-06-24 14:51:31 +0530

    set roboshop m

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -25,14 +25,15 @@ def call() {
 
         stage('quality control') {
           environment {
-           sonar_user = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
-           sonar_pass = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
+           SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
+           //SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
           }
 
           steps {
             script {
-              wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${sonar_pass}", var: 'secret']]]) {
-                sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${sonar_user} -Dsonar.password=${sonar_pass} -Dsonar.projectKey=cart"
+              SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\'', returnStdout: true).trim()
+              wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'secret']]]) {
+                sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
 
               }
 
