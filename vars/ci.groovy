838f3224df46d48bec4847f1e83683907fc07eb0
gaddamrk <raj>
2023-06-27 18:38:12 +0530

    set roboshop m

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,54 +1,53 @@
 def call() {
-  if(!env.SONAR_EXTRA_OPTS)
-    env.SONAR_EXTRA_OPTS= " "
-
-  if(!env.TAG_NAME) {
-    env.PUSH_CODE = "false"
-  } else {
-    env.PUSH_CODE = "true"
+  if(!env.SONAR_EXTRA_OPTS) {
+      env.SONAR_EXTRA_OPTS = " "
   }
-
-
+  if(!env.TAG_NAME) {
+      env.PUSH_CODE = "false"
+  } else {
+      env.PUSH_CODE = "true"
+  }
   try {
+
     node('workstation') {
 
-      stage('check out') {
-        cleanWs()
-        git branch: 'main', url: "https://github.com/gaddamrk/${component}.git"
-        sh 'env'
-      }
-      stage('compile/build') {
-        common.compile()
-      }
-      stage('unit test') {
-        common.unittests()
-      }
-      stage('quality control') {
+        stage('check out') {
+            cleanWs()
+            git branch: 'main', url: "https://github.com/gaddamrk/${component}.git"
+            sh 'env'
+        }
+        stage('compile/build') {
+            common.compile()
+        }
+        stage('unit test') {
+            common.unittests()
+        }
+        stage('quality control') {
 
-          SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\'', returnStdout: true).trim()
-          SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\'', returnStdout: true).trim()
-          wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-            sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
-          }
-      }
-
-      if (env.PUSH_CODE == "true") {
-        stage('upload code to centralized place') {
-          echo 'upload file'
-
+            SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption | sed \'s/"//g\'', returnStdout: true).trim()
+            SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.password --query Parameters[0].Value --with-decryption | sed \'s/"//g\'', returnStdout: true).trim()
+            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                sh "sonar-scanner -Dsonar.host.url=http://172.31.85.30:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+            }
         }
 
-
-      }
+        if(env.PUSH_CODE == "true") {
+          stage('upload code to centralized place') {
+              echo 'upload file'
+          }
+        }
 
 
     }
 
-  } catch (Exception ignored) {
+
+  } catch(Exception e) {
     common.email('failed')
   }
 
 }
+
+
 
 
 
