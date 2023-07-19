514e6ae0d89011d81e610785adc5d3b0939c04ce
gaddamrk <raj>
2023-07-19 15:46:14 +0530

    jr

---
+++ vars/terraform.groovy
@@ -0,0 +1,20 @@
+def call() {
+    pipeline {
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+        parameters {
+            string(name: 'INFRA_ENV', defaultValue: '', description: 'enter env like dev or prod')
+        }
+        stages {
+
+            stage('terraform init') {
+                steps {
+                    sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+                }
+            }
+        }
+    }
+}
